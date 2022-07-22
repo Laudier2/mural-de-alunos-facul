46a9173db40f4f2f421eb40ed37a0d907aa5c7c1
Laudier2 <trabalhandooline2019@hotmail.com>
2022-07-22 20:38:45 -0300

    Novo update

--- src/components/navBar/nav2.jsx
+++ src/components/navBar/nav2.jsx
@@ -2,6 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { BsGithub } from 'react-icons/bs'
+import { TiSocialYoutubeCircular } from 'react-icons/ti'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { FcPortraitMode } from 'react-icons/fc'
 
 function BasicExample() {
 
@@ -12,7 +16,7 @@ function BasicExample() {
     const email = JSON.parse(localStorage.getItem("user"))
 
     return (
-        <Navbar bg="white" expand="lg"  className="text-white mt-2">
+        <Navbar bg="white" expand="lg"  className="text-white mt-2 navbar-dark">
             <Container>
                 <Navbar.Brand href="/" className="titolo3 text-white">
                     
@@ -32,27 +36,26 @@ function BasicExample() {
                         <NavDropdown title="Mais" id="basic-nav-dropdown">
                             <NavDropdown.Item href="https://www.youtube.com/channel/UCk8HdZCe8RFMkZqVIQ0mL3g">
 
-                                <i className="fa-brands fa-youtube text-danger ml-2"></i>
-                                <strong className="bg-white">
-                                    Youtube
-                                </strong>
+                                <TiSocialYoutubeCircular className="bg-white h3 text-danger" />
+                                Youtube
+                                
                             </NavDropdown.Item>
                             <NavDropdown.Item href="https://github.com/Laudier2">
                                 
-                                    <i className="fa-brands fa-github-alt text-white ml-2"></i>
+                                    <BsGithub className="bg-white m-1 h" />
                                 
                                 GitHub
                             </NavDropdown.Item>
                             <NavDropdown.Item href="https://www.linkedin.com/in/jos%C3%A9-santana-de-jesus-8949b3124/">
                                 
-                                    <i className="fa-brands fa-linkedin text-primary"></i>
+                                <AiFillLinkedin className="bg-white text-primary m-1 h" />
                                
                                 LinkDin
                             </NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="https://laudierstdev.ga/">
                                 
-                                    <i className="fa-solid fa-passport"></i>
+                                <FcPortraitMode className="bg-white m-1 h" />
                                 
                                 Portifólio
                             </NavDropdown.Item>
